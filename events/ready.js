4226c3f2e1a065333bc154fcb532e65014b98749
Slash <74565595+Slash-2548@users.noreply.github.com>
2021-01-16 22:58:12 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,5 @@
-module.exports = async(client) => {
-
-client.user.setActivity("Slash#2548", {type: "WATCHING"});
+module.exports = async (client) => {
+    client.user.setActivity("Slash#2548", {
+        type: "WATCHING"
+    });
 }
-
-
-
-
