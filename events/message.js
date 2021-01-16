d65043512c2840ce028c8162eec8e8fc63e94aa8
Slash <74565595+Slash-2548@users.noreply.github.com>
2021-01-16 22:58:00 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,26 +1,22 @@
-const Discord = require("discord.js");
-const prefix = require('../config.json');
-const config = require('../config.json');
+    const Discord = require("discord.js");
+
+    module.exports = async (client, message) => {
+
+        const {
+            prefix
+        } = require('../config.json');
 
 
-module.exports = async(client, message) => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
+        if (message.author.bot) return;
+        if (message.channel.type === "dm") return;
+        if (!message.content.startsWith(prefix)) return;
 
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        const commande = args.shift();
+        const cmd = client.commands.get(commande);
 
-    if(!message.content.startsWith(config.prefix)) return;
+        if (!cmd) return;
+        message.delete();
 
-    let color = config.color
-    let colorerror = config.colorError
-    let colorsucces = config.colorSucces
-
-
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const commande = args.shift();
-    const cmd = client.commands.get(commande);
-    if(!cmd) return;
-
-    message.delete();
-
-    cmd.run(client, message, args, color, colorerror, colorsucces);
-};
+        cmd.run(client, message, args);
+    };
