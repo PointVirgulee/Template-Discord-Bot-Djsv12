e2bd5add2e23748267743479230055ca8d06b645
Slash <74565595+Slash-2548@users.noreply.github.com>
2021-01-16 22:57:10 +0100

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,22 +1,25 @@
-const Discord = require('discord.js')
+  const Discord = require('discord.js')
 
-module.exports.run = async(client, message, args, color) => {
+  module.exports.run = async (client, message, args) => {
 
-  let pingEmbed = new Discord.MessageEmbed()
-  pingEmbed.setDescription(`:robot: **Ping en cours...**`)
-  pingEmbed.setColor(color)
+    const {
+      color
+    } = require('../config.json');
 
-  const m = await message.channel.send(pingEmbed);
-  let ping = m.createdTimestamp - message.createdTimestamp;
-  
-  let pongEmbed = new Discord.MessageEmbed()
-  pongEmbed.setDescription(`:robot: La latence du bot est de **${ping}ms**`)
-  pongEmbed.setColor(color)
 
-  m.edit(pongEmbed);
-}
-            
-module.exports.help = {
-    name: "ping"
+    const pingEmbed = new Discord.MessageEmbed()
+    pingEmbed.setDescription(`:robot: **Ping en cours...**`)
+    pingEmbed.setColor(color)
+
+    const m = await message.channel.send(pingEmbed);
+    const ping = m.createdTimestamp - message.createdTimestamp;
+
+    const pongEmbed = new Discord.MessageEmbed()
+    pongEmbed.setDescription(`:robot: La latence du bot est de **${ping}ms**`)
+    pongEmbed.setColor(color)
+    m.edit(pongEmbed);
   }
-  
+  module.exports.help = {
+    name: "ping",
+    category: "autre"
+  }
