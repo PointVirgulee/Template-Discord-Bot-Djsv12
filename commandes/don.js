591ea08d2e0e251c21b9fe22f43c8ceeec26211d
Slash <74565595+Slash-2548@users.noreply.github.com>
2021-01-16 22:56:04 +0100

    Update don.js

--- commandes/don.js
+++ commandes/don.js
@@ -1,11 +1,8 @@
-    const Discord = require('discord.js')
-
     module.exports.run = async (client, message, args, color) => {
 
-    message.channel.send("**__Vous souhaitez m'offrir un petit café ?__**\n▸ <https://paypal.me/rideproject>");
-        
-}
-    
-module.exports.help = {
-    name: "don"
-}
+        message.channel.send("**__Vous souhaitez m'offrir un petit café ?__**\n▸ <https://paypal.me/rideproject>")
+    }
+    module.exports.help = {
+        name: "don",
+        category: "autre"
+    }
