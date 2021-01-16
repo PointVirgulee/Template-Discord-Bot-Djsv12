184c6c4af705f8838d88ed87e21bddb07f1e8a53
Slash <74565595+Slash-2548@users.noreply.github.com>
2021-01-16 22:57:22 +0100

    Update say.js

--- commandes/say.js
+++ commandes/say.js
@@ -1,31 +1,33 @@
-const Discord = require('discord.js');
-const config = require('../config.json');
+    const Discord = require('discord.js');
 
-module.exports.run = async(client, message, args, color, colorerror) => {
+    module.exports.run = async (client, message, args) => {
+
+        const {
+            colorError
+        } = require('../config.json');
 
 
-    const tosay = args.join(" ")
+        const tosay = args.join(" ")
 
-	let notperm = new Discord.MessageEmbed();
-	notperm.setAuthor('Erreur')
-	notperm.setColor(colorerror)
-	notperm.setDescription(`:x: Vous n'avez pas la permission de me faire parler`)
+        if (!message.guild.member(message.author).hasPermission("MENTION_EVERYONE")) {
+            const notperm = new Discord.MessageEmbed()
+            notperm.setAuthor('Erreur')
+            notperm.setColor(colorError)
+            notperm.setDescription(`:x: Vous n'avez pas la permission de me faire parler`)
+            return message.channel.send(notperm)
+        }
 
-    if(!message.guild.member(message.author).hasPermission("MENTION_EVERYONE")) return message.channel.send(notperm);
-   
-    let notargs = new Discord.MessageEmbed();
-    notargs.setAuthor('Erreur')
-    notargs.setColor(colorerror)
-    notargs.setDescription(`:x: Vous n'avez pas précisé d'argument(s)`)
+        if (!tosay) {
+            const notargs = new Discord.MessageEmbed();
+            notargs.setAuthor('Erreur')
+            notargs.setColor(colorError)
+            notargs.setDescription(`:x: Vous n'avez pas précisé d'argument(s)`)
+            return message.channel.send(notargs)
+        }
 
-    
-
-if(!tosay){
-      return message.channel.send(notargs)
-}
-
-    message.channel.send(tosay);
-}
+        message.channel.send(tosay);
+    }
     module.exports.help = {
-    name: "say"
-}
+        name: "say",
+        category: "fun"
+    }
