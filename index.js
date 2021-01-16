6bb53581fbe91a9aa8c69c2122ae488df154ce3c
Slash <74565595+Slash-2548@users.noreply.github.com>
2021-01-16 22:54:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-const client = new Discord.Client({disableEveryone: true,});
-client.commands = new Discord.Collection();
-const config = require('./config.json');
+    //Des correctifs sont régulièrement postés, soyez vigilant.
+    
+    const Discord = require('discord.js');
+    const fs = require('fs');
+    const client = new Discord.Client({
+        disableEveryone: true,
+    });
+    client.commands = new Discord.Collection();
+    const config = require('./config.json');
 
     fs.readdir("./commandes/", (error, f) => {
-     if(error) console.log(error);
-     let Commandes = f.filter(f => f.split(".").pop() === "js");
-     if(Commandes.length <= 0) return console.log("Aucune commande trouvé !");
-     Commandes.forEach((f) => {let commande = require(`./commandes/${f}`);
-     console.log(`${f} commande chargée !`);
-     client.commands.set(commande.help.name, commande);});});
+        if (error) console.log(error);
+        let Commandes = f.filter(f => f.split(".").pop() === "js");
+        if (Commandes.length <= 0) return console.log("Aucune commande trouvé !");
+        Commandes.forEach((f) => {
+            let commande = require(`./commandes/${f}`);
+            console.log(`${f} commande chargée !`);
+            client.commands.set(commande.help.name, commande);
+        });
+    });
 
     fs.readdir("./events/", (error, f) => {
-     if(error) console.log(error);
-     console.log(`${f.length} events en chargement`);
-     f.forEach((f) => {const events = require(`./events/${f}`);
-     const event = f.split(".")[0];
-     client.on(event, events.bind(null, client));});});
+        if (error) console.log(error);
+        console.log(`${f.length} events en chargement`);
+        f.forEach((f) => {
+            const events = require(`./events/${f}`);
+            const event = f.split(".")[0];
+            client.on(event, events.bind(null, client));
+        });
+    });
 
-     client.login(config.token);
+    client.login(config.token);
