c153b024d6227f6feed34877b01f294794f0a23b
Point;Virgule#2548 <74565595+PointVirgulee@users.noreply.github.com>
2021-06-13 12:27:34 +0200

    Update owner.js

--- commandes/owner.js
+++ commandes/owner.js
@@ -1,7 +1,7 @@
 //Cette commande a été créée pour promouvoir mon travail.
 //Vous avez la possibilité de la supprimer, comme de la laisser.
 //Pour plus d'informations cercenant la template,
-//Voici mon Discord : Slash#2548.
+//Voici mon Discord : Point;Virgule#2548.
 
 
 const Discord = require('discord.js');
@@ -17,12 +17,12 @@ module.exports.run = (client, message, args) => {
     embed.setAuthor('Project Owner')
     embed.setThumbnail('https://zupimages.net/up/21/02/kf4n.png')
     embed.setColor(color)
-    embed.addField('Pseudo :', `Slash`, true)
+    embed.addField('Pseudo :', `Point;Virgule`, true)
     embed.addField('Tag :', '#2548', true)
     embed.addField('ID :', '423155238082576394', true)
-    embed.addField('Réseaux :', 'Github : https://github.com/Slash-2548\nTwitter : https://twitter.com/slash2548\nPaypal : https://paypal.me/rideproject', false)
+    embed.addField('Réseaux :', 'Github : https://github.com/PointVirgulee\nTwitter : https://twitter.com/slash2548\nPaypal : https://paypal.me/rideproject', false)
     embed.addField('Projet Perso :', 'Ride Support : https://discord.gg/W2yH7qE5sF\nRide Bot : https://discord.com/oauth2/authorize?client_id=714238332888023041&permissions=-1&scope=bot', false)
-    embed.setFooter('Créé et distribué par Slash#2548')
+    embed.setFooter('Créé et distribué par Point;Virgule#2548')
 
     message.channel.send(embed);
 
